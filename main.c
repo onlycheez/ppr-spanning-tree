f7d9dc96cfe2bfe9acf11fe61e50e4c4f3ea8023
Jan Sidlo <sidlojan@fit.cvut.cz>
2015-12-07 14:24:13 +0100

    Interprocess communication

--- main.c
+++ main.c
@@ -9,13 +9,137 @@
 
 #define TAG_REQUEST_DATA 1
 #define TAG_DATA 2
-#define TAG_IS_FINISHED 3
-#define TAG_FINISH 4
+#define TAG_ARE_YOU_FINISHED 3
+#define TAG_MY_FINISH_STATUS 4
+#define TAG_FINISH 5
 
-#define MSG_LENGTH 128
+#define MSG_LENGTH 1024 * 1024
+#define MAX_IPC_LENGTH 5000
+
+typedef struct _mpi_msg
+{
+  int length;
+  char data[MSG_LENGTH];
+} mpi_msg;
+
+#define MSG_FINSIHED 1
+#define MSG_NOT_FINSIHED 2
+
+mpi_msg* message_serialize_state(char is_finished)
+{
+  mpi_msg *message = malloc(sizeof(mpi_msg));
+
+  message->data[0] = is_finished;
+  message->data[1] = 0;
+
+  return message;
+}
+
+mpi_msg* message_serialize_data(list_node *root)
+{
+  mpi_msg *message = malloc(sizeof(mpi_msg));
+
+  if (root == NULL)
+  {
+    message->length = 1;
+    message->data[0] = -1;
+    message->data[1] = 0;
+  }
+  else
+  {
+
+  }
+
+  return message;
+}
+
+list_node* message_deserialize_data()
+{
+  return NULL;
+}
+
+int message_is_finished()
+{
+  return 0;
+}
+
+list_node* cut_stack(list_node *root)
+{
+  int size = list_size(root);
+
+  if (size < 10)
+  {
+    return NULL;
+  }
+
+  int cut_idx = size / 2;
+
+  if (cut_idx > MAX_IPC_LENGTH)
+  {
+    cut_idx = size - MAX_IPC_LENGTH;
+  }
+
+  list_node *tmp = list_at_index(root, cut_idx);
+  list_node *new_list = tmp->next;
+  tmp->next = NULL;
+
+  return new_list;
+}
+
+int _recursion_count = 0;
 
 void find_solution(list_node** solutions, list_node* current_solution, int size)
 {
+  _recursion_count += 1;
+  if (_recursion_count == 50)
+  {
+    _recursion_count = 0;
+
+    MPI_Status status;
+    mpi_msg *message;
+    char buffer[MSG_LENGTH];
+
+    int flag = 0;
+    MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
+
+    while (flag)
+    {
+      MPI_Recv(&buffer, MSG_LENGTH, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG,
+        MPI_COMM_WORLD, &status);
+
+      switch (status.MPI_TAG)
+      {
+        case TAG_REQUEST_DATA:
+        {
+          list_node *new_top = cut_stack(*solutions);
+          message = message_serialize_data(new_top);
+          MPI_Send(&message->data, message->length, MPI_CHAR, status.MPI_SOURCE,
+            TAG_DATA, MPI_COMM_WORLD);
+          free(message);
+          list_free(new_top);
+          break;
+        }
+        case TAG_DATA:
+          printf("find_solution: received data from %d\n", status.MPI_SOURCE);
+          break;
+        case TAG_ARE_YOU_FINISHED:
+          message = message_serialize_state(MSG_NOT_FINSIHED);
+          MPI_Send(&message->data, message->length, MPI_CHAR, status.MPI_SOURCE,
+            TAG_MY_FINISH_STATUS, MPI_COMM_WORLD);
+          free(message);
+          break;
+        case TAG_FINISH:
+          printf("find_solution: received request to finish from %d\n",
+            status.MPI_SOURCE);
+          break;
+        default:
+          printf("Unhandled switch branch - %d\n", status.MPI_TAG);
+      }
+
+      MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
+    }
+  }
+
   int current_solution_node_idx = list_size(current_solution) - size;
   list_node *current_solution_node = list_at_index(current_solution,
     current_solution_node_idx);
@@ -102,39 +226,24 @@ void mark_nodes_unvisited(list_node *root)
 double best_height = 1000.0;
 list_node *best_solution;
 
-list_node* message_serialize()
-{
-  return NULL;
-}
-
-list_node* message_deserialize()
-{
-  return NULL;
-}
-
-int message_is_finished()
-{
-  return NULL;
-}
-
 int are_others_finished(int p)
 {
   int finished_count = 0;
   MPI_Status status;
-  char message[1];
+  char buffer[2];
   int i;
 
   for (i = 1; i < p; i++)
   {
-    MPI_Send(NULL, 0, MPI_CHAR, p, TAG_IS_FINISHED, MPI_COMM_WORLD);
+    MPI_Send(NULL, 0, MPI_CHAR, p, TAG_ARE_YOU_FINISHED, MPI_COMM_WORLD);
   }
 
   for (i = 1; i < p; i++)
   {
-    MPI_Recv(&message, MSG_LENGTH, MPI_CHAR, MPI_ANY_SOURCE,
-        TAG_IS_FINISHED, MPI_COMM_WORLD, &status);
+    MPI_Recv(&buffer, MSG_LENGTH, MPI_CHAR, MPI_ANY_SOURCE,
+        TAG_MY_FINISH_STATUS, MPI_COMM_WORLD, &status);
 
-    if (message_is_finished(message))
+    if (message_is_finished(buffer))
     {
       finished_count += 1;
     }
@@ -143,20 +252,41 @@ int are_others_finished(int p)
   return finished_count == (p - 1);
 }
 
+int _last_asked;
+
+int last_asked(int p, int my_rank)
+{
+  _last_asked += 1;
+
+  if (_last_asked == my_rank)
+  {
+    _last_asked += 1;
+  }
+
+  if (_last_asked >= p)
+  {
+    _last_asked = 0;
+  }
+
+  return _last_asked;
+}
+
 int main(int argc, char *argv[])
 {
-  char message[MSG_LENGTH];
+  char buffer[MSG_LENGTH];
   MPI_Status status;
   int my_rank, p;
   MPI_Init(&argc, &argv);
   MPI_Comm_rank(MPI_COMM_WORLD, &my_rank);
   MPI_Comm_size(MPI_COMM_WORLD, &p);
+  _last_asked = my_rank;
 
   struct graph *graph = graph_new_from_file(argv[1]);
   int ideal_height = ceilf(log2(graph->size)) + 1;
 
   list_node *solution;
   list_node *solutions;
+  int repeat = 1;
 
   if (my_rank == 0)
   {
@@ -166,12 +296,50 @@ int main(int argc, char *argv[])
   else
   {
     MPI_Send(NULL, 0, MPI_CHAR, 0, TAG_REQUEST_DATA, MPI_COMM_WORLD);
-    MPI_Recv(&message, MSG_LENGTH, MPI_CHAR, 0, TAG_DATA, MPI_COMM_WORLD,
-      &status);
-    solutions = message_deserialize(message);
-  }
+    mpi_msg *message;
 
-  int repeat = 1;
+    int repeat_probe = 1;
+    while (repeat_probe)
+    {
+      MPI_Recv(&buffer, MSG_LENGTH, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG,
+        MPI_COMM_WORLD, &status);
+
+      switch (status.MPI_TAG)
+      {
+        case TAG_REQUEST_DATA:
+          printf("p %d received data request\n", p);
+          message = message_serialize_data(NULL);
+          MPI_Send(&message->data, message->length, MPI_CHAR, status.MPI_SOURCE,
+            TAG_DATA, MPI_COMM_WORLD);
+          free(message);
+          break;
+        case TAG_DATA:
+          solutions = message_deserialize_data(buffer);
+          if (solutions == NULL)
+          {
+            MPI_Send(NULL, 0, MPI_CHAR, last_asked(p, my_rank),
+              TAG_REQUEST_DATA, MPI_COMM_WORLD);
+          }
+          else
+          {
+            repeat_probe = 0;
+          }
+          break;
+        case TAG_ARE_YOU_FINISHED:
+          message = message_serialize_state(MSG_FINSIHED);
+          MPI_Send(&message->data, message->length, MPI_CHAR, status.MPI_SOURCE,
+            TAG_MY_FINISH_STATUS, MPI_COMM_WORLD);
+          free(message);
+          break;
+        case TAG_FINISH:
+          repeat = 0;
+          repeat_probe = 0;
+          break;
+        default:
+          printf("Unhandled switch branch - %d\n", status.MPI_TAG);
+      }
+    }
+  }
 
   while (repeat)
   {
@@ -222,17 +390,55 @@ int main(int argc, char *argv[])
     else
     {
       int flag = 0;
-      MPI_Iprobe(0, TAG_FINISH, MPI_COMM_WORLD, &flag, &status);
-      if (flag)
+      MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
+
+      while (flag)
       {
-        break;
+        MPI_Recv(&buffer, MSG_LENGTH, MPI_CHAR, MPI_ANY_SOURCE, MPI_ANY_TAG,
+            MPI_COMM_WORLD, &status);
+
+        switch (status.MPI_TAG)
+        {
+          case TAG_REQUEST_DATA:
+          {
+            mpi_msg *message = message_serialize_data(NULL);
+            MPI_Send(&message->data, message->length, MPI_CHAR,
+              status.MPI_SOURCE, TAG_DATA, MPI_COMM_WORLD);
+            free(message);
+            break;
+          }
+          case TAG_DATA:
+            printf("p %d received data from %d\n", my_rank, status.MPI_SOURCE);
+            break;
+          case TAG_ARE_YOU_FINISHED:
+          {
+            mpi_msg *message = message_serialize_state(MSG_FINSIHED);
+            MPI_Send(&message->data, message->length, MPI_CHAR,
+              status.MPI_SOURCE, TAG_MY_FINISH_STATUS, MPI_COMM_WORLD);
+            free(message);
+            break;
+          }
+          case TAG_FINISH:
+            repeat = 0;
+            break;
+          default:
+            printf("Unhandled switch branch - %d\n", status.MPI_TAG);
+        }
+
+        MPI_Iprobe(MPI_ANY_SOURCE, MPI_ANY_TAG, MPI_COMM_WORLD, &flag, &status);
       }
     }
 
-    MPI_Send(NULL, 0, MPI_CHAR, 0, TAG_REQUEST_DATA, MPI_COMM_WORLD);
-    MPI_Recv(&message, MSG_LENGTH, MPI_CHAR, 0, TAG_DATA, MPI_COMM_WORLD,
+    if (!repeat)
+    {
+      break;
+    }
+
+    MPI_Send(NULL, 0, MPI_CHAR, last_asked(p, my_rank), TAG_REQUEST_DATA,
+      MPI_COMM_WORLD);
+    MPI_Recv(&buffer, MSG_LENGTH, MPI_CHAR, 0, TAG_DATA, MPI_COMM_WORLD,
       &status);
-    solutions = message_deserialize(message);
+    solutions = message_deserialize_data(buffer);
   }
 
   dump_solution(best_solution);
